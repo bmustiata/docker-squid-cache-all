b193e70861c58673b717e13e3e5137536b87122f
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-03-30 22:38:19 +0200

    Added deb and rpm files into the fray, describe the usage of the certificate.

--- bin/run-local.sh
+++ bin/run-local.sh
@@ -1,4 +1,4 @@
 docker kill squid-proxy
 docker rm -v squid-proxy
-docker run -d --name squid-proxy -v /opt/squid/cache:/var/spool/squid3:rw -v /opt/squid/cert:/etc/squid3/ssl_cert:rw bmst/docker-squid-cache-all
+docker run -d --name squid-proxy -v /opt/squid/cache:/var/spool/squid3:rw -v /opt/squid/cert:/etc/squid3/ssl_cert:rw -p 3128:3128 bmst/docker-squid-cache-all
 
